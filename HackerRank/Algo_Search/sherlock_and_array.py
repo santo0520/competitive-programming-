c9b6f05f258d18c82588b37ffc13f3472f59ce71
santo0520 <coolsanderslin@gmail.com>
2015-09-02 01:40:23 +0800

    Update sherlock_and_array.py

--- HackerRank/Algo_Search/sherlock_and_array.py
+++ HackerRank/Algo_Search/sherlock_and_array.py
@@ -1,1 +1,21 @@
+def solve(arr,len):
+    """given sequence of array, return yes/no"""
+    left_sum = 0
+    right_sum= sum(arr[1:])
+    #remenber to test edge case
+    if left_sum == right_sum:
+        return "YES"
+    else:
+        for idx in range(1,len):
+            left_sum += arr[idx-1]
+            right_sum -= arr[idx]
+            if left_sum == right_sum:
+                return "YES"
+        return "NO"
 
+#main
+num_cases = int(raw_input())
+for _ in range(num_cases):
+    len = int(raw_input())
+    arr = [int(i) for i in raw_input().split(" ")]
+    print solve(arr,len)
